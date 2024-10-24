4eaedf59ccfe9a08fd8ff5b6f0b799b243e3f87f
vishal <vishallengare61@gmail.com>
2024-10-24 10:30:17 +0530

    working for the code optimmization

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -18,13 +18,8 @@ const Sidebar = ({ isCollapsed, toggleSidebar }) => {
     <div className={`${styles.sidebar} ${isCollapsed ? styles.collapsed : ""}`}>
       <ul className={styles.sidebarItems}>
         <li>
-          <NavLink
-            to="/dashboard"
-            className={({ isActive }) =>
-              isActive
-                ? `${styles.navLink} ${styles.activeLink}`
-                : styles.navLink
-            } >
+          <NavLink to="/dashboard" className={({ isActive }) =>
+              isActive ? `${styles.navLink} ${styles.activeLink}` : styles.navLink } >
             <FaTachometerAlt className={styles.icon} />
             {!isCollapsed && <span>Dashboard</span>}
           </NavLink>
@@ -40,124 +35,67 @@ const Sidebar = ({ isCollapsed, toggleSidebar }) => {
               {openMenu === "master" ? <FaChevronDown /> : <FaChevronRight />}{" "}
             </div>
           </div>
-          <ul
-            className={`${styles.subMenu} ${
-              openMenu === "master" ? styles.open : ""
-            }`}
-          >
+          <ul className={`${styles.subMenu} ${ openMenu === "master" ? styles.open : "" }`} >
             <li>
-              <NavLink
-                to="/manageUserRole"
+              <NavLink to="/manageUserRole"
                 className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaUsers className={styles.icon} />
                 {!isCollapsed && <span>Manage User Role</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/manageUser"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/manageUser" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaUserEdit className={styles.icon} />
                 {!isCollapsed && <span>Manage Users</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/manageProjects"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/manageProjects" className={({ isActive }) =>
+                  isActive ? `${ styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaClipboardList className={styles.icon} />
                 {!isCollapsed && <span>Manage Project</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/activities"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/activities" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaTasks className={styles.icon} />
                 {!isCollapsed && <span>Manage Activities</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/area"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/area" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaMapMarkedAlt className={styles.icon} />
                 {!isCollapsed && <span>Manage Area</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/subarea"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/subarea" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaFolderOpen className={styles.icon} />
                 {!isCollapsed && <span>Manage Sub Area</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/manageSnapBlock"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/manageSnapBlock" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaUpload className={styles.icon} />
                 {!isCollapsed && <span>Manage Snap Block Update</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/uploadAPK"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/uploadAPK" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaUpload className={styles.icon} />
                 {!isCollapsed && <span>Upload APK</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/uploadKMZ"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/uploadKMZ" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaUpload className={styles.icon} />
                 {!isCollapsed && <span>Upload KMZ File</span>}
               </NavLink>
@@ -166,10 +104,7 @@ const Sidebar = ({ isCollapsed, toggleSidebar }) => {
         </li>
 
         <li>
-          <div
-            onClick={() => toggleMenu("settings")}
-            className={styles.menuItem}
-          >
+          <div onClick={() => toggleMenu("settings")} className={styles.menuItem} >
             <div className={styles.menuItemContent}>
               <FaCog className={styles.icon} />
               <span>{!isCollapsed ? "Settings" : null}</span>
@@ -178,85 +113,45 @@ const Sidebar = ({ isCollapsed, toggleSidebar }) => {
               {openMenu === "settings" ? <FaChevronDown /> : <FaChevronRight />}{" "}
             </div>
           </div>
-          <ul
-            className={`${styles.subMenu} ${
-              openMenu === "settings" ? styles.open : ""
-            }`}
-          >
+          <ul className={`${styles.subMenu} ${ openMenu === "settings" ? styles.open : "" }`} >
             <li>
-              <NavLink
-                to="/projectSites"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/projectSites" className={({ isActive }) =>
+                   isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaMapMarkedAlt className={styles.icon} /> {/* Updated Icon */}
                 {!isCollapsed && <span>View Project Sites</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/geomapping"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/geomapping" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaLock className={styles.icon} />
                 {!isCollapsed && <span>Sites Geomapping</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/dailyProjects"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/dailyProjects" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaCalendarDay className={styles.icon} /> {/* Updated Icon */}
                 {!isCollapsed && <span>Daily Project Sites</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/daywise"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/daywise" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaCalendarAlt className={styles.icon} /> {/* Updated Icon */}
                 {!isCollapsed && <span>Daywise Project Sites</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/processedImages"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/processedImages" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaImage className={styles.icon} /> {/* Updated Icon */}
                 {!isCollapsed && <span>Processed Images</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/userreport"
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.subMenuLink} ${styles.activeLink}`
-                    : styles.subMenuLink
-                }
-              >
+              <NavLink to="/userreport" className={({ isActive }) =>
+                  isActive ? `${styles.subMenuLink} ${styles.activeLink}` : styles.subMenuLink } >
                 <FaFileAlt className={styles.icon} /> {/* Updated Icon */}
                 {!isCollapsed && <span>User Report</span>}
               </NavLink>
